745f11faa2dca9957743cc44898f5ba3f59de0be
Icorp <itteam1602@gmail.com>
2020-11-04 10:01:21 +0600

    delete pycashe directory and add gitignnore

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Category ...
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+# Posts ...
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=False)
+    last_modified = models.DateTimeField(auto_now=False)
+    categories = models.ManyToManyField("Category",related_name='posts')
